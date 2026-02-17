b44426ccba2dfa41317671b5c41073a34817da3e
Rincy Pradeep <rincypradeep15@gmail.com>
2026-02-17 17:38:14 +0530

    sign up and sign in

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,22 @@ import Dashboard from './pages/Dashboard'
 import FoodLog from './pages/FoodLog'
 import ActivityLog from './pages/ActivityLog'
 import Profile from './pages/Profile'
+import { useAppContext } from './context/AppContext'
+import Login from './pages/Login'
+import Loading from './components/Loading'
+import Onboarding from './pages/Onboarding'
 
 const App = () => {
+  const {user, isUserFetched, onboardingCompleted} = useAppContext()
+
+  if(!user){
+    return isUserFetched ? <Login /> : <Loading />
+  }
+
+  if(!onboardingCompleted){
+    return <Onboarding />
+  }
+
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
